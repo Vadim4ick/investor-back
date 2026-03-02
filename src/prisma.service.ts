a6084a87c8037db786cb61dd0bdc22491c1cca7c
Vadim <a-yva-2013@mail.ru>
2026-03-02 22:12:07 +0400

    add prisma

---
+++ src/prisma.service.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+import 'dotenv/config';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    if (!connectionString) throw new Error('DATABASE_URL is not defined');
+
+    const pool = new Pool({ connectionString });
+
+    // ✅ PrismaPg — constructor, поэтому new
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,13 @@
+// dto/user.dto.ts
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserDto {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: 'vadim@example.com' })
+  email: string;
+
+  @ApiProperty({ example: 'Vadim', required: false, nullable: true })
+  username?: string | null;
+}
